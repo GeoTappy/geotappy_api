2f60f351994e7f841be2a9d48beacb73f0fe1f43
Konrad Oleksiuk <konrad@up-next.com>
2014-11-13 11:23:54 +0100

    Accept hash for LocationShareJob

--- app/jobs/location_share_job.rb
+++ app/jobs/location_share_job.rb
@@ -1,7 +1,7 @@
 class LocationShareJob
   include SuckerPunch::Job
 
-  def perform(*args)
+  def perform(args = {})
     SuckerPunch.logger.debug args
 
     ActiveRecord::Base.connection_pool.with_connection do
