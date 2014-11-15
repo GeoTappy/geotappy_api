6ff4254e4298b8f3552fb7d037bf9f457992ca92
Konrad Oleksiuk <konrad@up-next.com>
2014-11-15 22:30:18 +0100

    Fix Redis health check, add version to status

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+require_relative 'version'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
---
+++ config/version.rb
@@ -0,0 +1,8 @@
+module GeotappyApi
+  MAJOR = 1
+  MINOR = 0
+  TINY  = 0
+  PRE   = "pre"
+
+  VERSION = [MAJOR, MINOR, TINY, PRE].compact.join(".")
+end
