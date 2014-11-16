6d6b8f892b8c0eb941409ea512ddafc47e1f01d9
Konrad Oleksiuk <konrad@up-next.com>
2014-11-16 22:47:16 +0100

    Configure own APNS solution

---
+++ app/services/apn_connection.rb
@@ -0,0 +1,37 @@
+class APNConnection
+  def initialize
+    setup
+  end
+
+  def setup
+    @uri = if Settings.apns.env == 'prod'
+             Houston::APPLE_PRODUCTION_GATEWAY_URI
+           else
+             Houston::APPLE_DEVELOPMENT_GATEWAY_URI
+           end
+
+    @certificate = File.read(File.join(Rails.root, 'certificates', Settings.apns.cert))
+    @passphrase = Settings.apns.passphrase
+
+    @connection = Houston::Connection.new(@uri, @certificate, @passphrase)
+    @connection.open
+  end
+
+  def write(data)
+    begin
+      raise "Connection is closed" unless @connection.open?
+      @connection.write(data)
+    rescue Exception => e
+      attempts ||= 0
+      attempts += 1
+
+      if attempts < 5
+        setup
+        retry
+      else
+        raise e
+      end
+    end
+  end
+
+end
